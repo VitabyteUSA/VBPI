fdc20d46c00be68aee3200e77eeb34a7532e6090
Mohamed Muflahi <muflahimohamed88@gmail.com>
2025-09-09 16:03:09 -0400

    remove discord

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -38,9 +38,7 @@
 							target="_blank">{$i18n.t('See readme.md for instructions')}</a
 						>
 						{$i18n.t('or')}
-						<a class=" font-semibold underline" href="https://discord.gg/5rJgQTnV4s" target="_blank"
-							>{$i18n.t('join our Discord for help.')}</a
-						>
+						
 					</div>
 
 					<div class=" mt-6 mx-auto relative group w-fit">
